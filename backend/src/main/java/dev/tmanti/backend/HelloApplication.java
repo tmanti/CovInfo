08f4f9e01ea388bcfbb063d1ac94cd34d20e63c4
tmanti <tmanti17@gmail.com>
2022-07-14 13:22:29 -0400

    get backend on a working framework (spring)

--- backend/src/main/java/dev/tmanti/backend/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package dev.tmanti.backend;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
